47613ab128e846cc5329f90481e44c1399df13db
15801997619 <1562412509@qq.com>
2019-04-19 14:57:10 +0800

    测试

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+/**
+ * @ClassName:Test
+ * @Description: TODO
+ * @Author: xu.liu
+ * @Date:2019/3/14 15:24
+ * @Version:V1.0
+ **/
+public class Test {
+
+    private String name;
+    private String sex;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
